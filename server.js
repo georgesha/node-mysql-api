5576f0faed19fd979823b8bc299b7add9581ddc0
zsha2 <shazjgeorge@gmail.com>
2018-02-02 15:28:23 -0500

    finish post method

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-var router =  express.Router();
+var router = express.Router();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  limit: '100mb',
+  extended: true
+}));
+app.use(bodyParser.json({
+  limit: '100mb'
+}));
 
 var port = process.env.PORT || 5000;
 
@@ -8,9 +17,26 @@ var pool = require("./db-pool.js");
 
 router.get('/todo', function(req, res) {
   pool.query('SELECT * FROM todo_table', function(err, rows) {
-    res.send(rows);
+    if (err) {
+      res.end("Error!");
+    } else {
+      res.send(rows);
+    }
   });
 });
 
-app.use('/api',router);
+router.post('/todo', function(req, res) {
+  var id = req.body.id;
+  var content = req.body.content;
+  var query = "INSERT INTO todo_table (id, content) VALUES ('" + id + "', '" + content + "')";
+  pool.query(query, function(err, rows) {
+    if (err) {
+      res.end("Error!");
+    } else {
+      res.send("New todo added");
+    }
+  });
+});
+
+app.use('/api', router);
 app.listen(port);

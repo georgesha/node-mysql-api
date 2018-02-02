ddd46bfdae8ab212197cf6aaac890ba7c83dd24a
zsha2 <shazjgeorge@gmail.com>
2018-02-02 15:56:24 -0500

    finish get single todo by id method

--- server.js
+++ server.js
@@ -16,10 +16,25 @@ var port = process.env.PORT || 5000;
 var pool = require("./db-pool.js");
 
 router.get('/todo', function(req, res) {
-  pool.query('SELECT * FROM todo_table', function(err, rows) {
+  var query = "SELECT * FROM todo_table";
+  pool.query(query, function(err, rows) {
     if (err) {
       res.end("Error!");
-    } else {
+    }
+    else {
+      res.send(rows);
+    }
+  });
+});
+
+router.get('/todo/:id', function(req, res) {
+  var id = req.params.id;
+  var query = "SELECT * FROM todo_table WHERE id='" + id + "'";
+  pool.query(query, function(err, rows) {
+    if (err) {
+      res.end("Error!");
+    }
+    else {
       res.send(rows);
     }
   });
@@ -32,7 +47,8 @@ router.post('/todo', function(req, res) {
   pool.query(query, function(err, rows) {
     if (err) {
       res.end("Error!");
-    } else {
+    }
+    else {
       res.send("New todo added");
     }
   });
